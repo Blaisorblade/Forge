ed44254e1524dbd02306689e7fcf8ca7a7787dec
AustinBGibbons <gibbons4@stanford.edu>
2013-01-17 01:51:47 -0800

    reduced quotation escaping

--- src/examples/OptiWrangle.scala
+++ src/examples/OptiWrangle.scala
@@ -18,10 +18,6 @@ trait OptiWrangle extends ForgeApplication with ScalaOps {
     //Types
     val T = tpePar("T")
     val DataWrangler = tpe("DataWrangler")
-    //val AS = tpeInst(GArray(T), List(MString))
-    //val ASS = tpeInst(GArray(T), List(AS))
-    //val AS = GArray(tpePar(MString)) //tpeInst(MString)
-    //val ASS = GArray(tpePar(AS)) //tpeInst(AS)
     val AS = tpeInst(GArray(T), List(MString))
     val ASS = tpeInst(GArray(T), List(AS))
 
@@ -44,16 +40,16 @@ trait OptiWrangle extends ForgeApplication with ScalaOps {
     // -- Datawrangler 
     codegen (dw_new) ($cala, stream.printLines("val table = scala.io.Source.fromFile("+dw_new.quotedArg(0)+").mkString.split("+dw_new.quotedArg(1)+").map(x=> x.split("+dw_new.quotedArg(2)+"))",
     "new "+dw_new.tpeName+"("+dw_new.quotedArg(0)+", "+dw_new.quotedArg(1)+", "+dw_new.quotedArg(2)+", table)"), isSimple=false)
-    codegen (dw_new_2) ($cala, "new "+dw_new_2.tpeName+"(\\\"\\\",\\\"\\\",\\\"\\\","+dw_new.quotedArg(0)+")")
+    codegen (dw_new_2) ($cala, "new "+dw_new_2.tpeName+"(\"\",\"\",\"\","+dw_new.quotedArg(0)+")")
 
     //todo - strings vs. regex - see what dw does
-    codegen (dw_cut) ($cala, "new "+dw_cut.tpeName+"(\\\"\\\",\\\"\\\",\\\"\\\","+dw_cut.quotedArg(0)+"._table map (y => y map (x => x.replaceAll("+dw_cut.quotedArg(1)+", \\\"\\\"))))")
-    codegen (dw_drop) ($cala, "new "+dw_cut.tpeName+"(\\\"\\\",\\\"\\\",\\\"\\\","+dw_drop.quotedArg(0)+"._table map (y => {val z = y.toBuffer ; z.remove("+dw_drop.quotedArg(1)+") ; z.toArray}))")
-    codegen (dw_delete) ($cala, "new "+dw_cut.tpeName+"(\\\"\\\",\\\"\\\",\\\"\\\","+dw_delete.quotedArg(0)+"._table filter (x => !(x("+dw_delete.quotedArg(1)+").indexOf("+dw_delete.quotedArg(2)+") == "+dw_delete.quotedArg(3)+")))")
+    codegen (dw_cut) ($cala, "new "+dw_cut.tpeName+"(\"\",\"\",\"\","+dw_cut.quotedArg(0)+"._table map (y => y map (x => x.replaceAll("+dw_cut.quotedArg(1)+", \"\"))))")
+    codegen (dw_drop) ($cala, "new "+dw_cut.tpeName+"(\"\",\"\",\"\","+dw_drop.quotedArg(0)+"._table map (y => {val z = y.toBuffer ; z.remove("+dw_drop.quotedArg(1)+") ; z.toArray}))")
+    codegen (dw_delete) ($cala, "new "+dw_cut.tpeName+"(\"\",\"\",\"\","+dw_delete.quotedArg(0)+"._table filter (x => !(x("+dw_delete.quotedArg(1)+").indexOf("+dw_delete.quotedArg(2)+") == "+dw_delete.quotedArg(3)+")))")
 
     codegen (dw_write_to_file) ($cala, stream.printLines("val outFile = new java.io.PrintStream(new java.io.FileOutputStream("+dw_write_to_file.quotedArg(1)+"))",
     dw_write_to_file.quotedArg(0)+"._table foreach { row =>",
-    "  outFile.println(row.mkString(\\\"\\\\\\\",\\\\\\\"\\\"))",
+    "  outFile.println(row.mkString(\"\\\",\\\"\"))",
     "}",
     "outFile.close()"), isSimple=false)
 
